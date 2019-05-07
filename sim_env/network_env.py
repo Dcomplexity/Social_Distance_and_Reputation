3763755bfe09b327e194f93eb5df4a831a4245ea
CHUANG DENG <cdengcnc@sjtu.edu.cn>
2019-05-07 14:15:11 +0800

    er_random and triangular_lattice

--- sim_env/network_env.py
+++ sim_env/network_env.py
@@ -25,6 +25,19 @@ def generate_network(structure, xdim=40, ydim=40, nodes_num=1600, edge_num=2):
             for j in range(len(adj_link[i])):
                 g_edge.add_edge(i, adj_link[i][j])
         return adj_link, population_num, g_edge.edges()
+    elif structure == "triangular_lattice":
+        g_network = nx.triangular_lattice_graph(xdim, ydim * 2, periodic=True)
+        adj_array = nx.to_numpy_array(g_network)
+        adj_link = []
+        population_num = 0
+        for i in range(adj_array.shape[0]):
+            adj_link.append(list(np.where(adj_array[i] == 1)[0]))
+        population_num = xdim * ydim
+        g_edge = nx.Graph()
+        for i in range(len(adj_link)):
+            for j in range(len(adj_link[i])):
+                g_edge.add_edge(i, adj_link[i][j])
+        return adj_link, population_num, g_edge.edges()
     elif structure == "ba_graph":
         g_network = nx.barabasi_albert_graph(n=nodes_num, m=edge_num)
         adj_array = nx.to_numpy_array(g_network)
@@ -43,13 +56,17 @@ def generate_network(structure, xdim=40, ydim=40, nodes_num=1600, edge_num=2):
 
 
 def generate_er_random_connected(degree, nodes_num):
-    g_network = nx.erdos_renyi_graph(nodes_num, p=degree/(nodes_num-1))
+    if degree == 6:
+        # seed=101 can ensure a connected network
+        g_network = nx.fast_gnp_random_graph(nodes_num, p=degree/(nodes_num-1), seed=101)
+    else:
+        g_network = nx.fast_gnp_random_graph(nodes_num, p=degree/(nodes_num-1))
     gen_num = 0
     while not nx.is_connected(g_network):
-        print(gen_num)
-        g_network = nx.erdos_renyi_graph(nodes_num, p=degree/(nodes_num-1))
+        rnd_num = np.random
+        g_network = nx.fast_gnp_random_graph(nodes_num, p=degree/(nodes_num-1), seed=rnd_num)
         gen_num += 1
-        if gen_num > 50:
+        if gen_num > 100:
             print("Not found connected network")
             return None
     adj_array = nx.to_numpy_array(g_network)
@@ -64,7 +81,7 @@ def generate_er_random_connected(degree, nodes_num):
     return adj_link, population_num, g_edge.edges()
 
 if __name__ == "__main__":
-    adj_link_r, population_num_r, g_edge_r = generate_network("ba_graph")
+    adj_link_r, population_num_r, g_edge_r = generate_network(structure='triangular_lattice')
     logger = create_logger("ba_graph", file_name="./logs/log_network.txt")
     logger.info(adj_link_r)
     logger.info(population_num_r)
